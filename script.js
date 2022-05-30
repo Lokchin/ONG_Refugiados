dddbcdc47d3eea30f24714313595c0846b60698f
Bruno S. Lokchin <48932138+Lokchin@users.noreply.github.com>
2022-05-30 18:28:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let image = document.getElementById('imge');
 let images = ['/Imagens/imagemcasa1.png', '/Imagens/imagemcasa2.jpeg', '/Imagens/imagemcasa3.jpeg']
-setInterval(function(){
+setInterval(async function(){
   let random = Math.floor(Math.random() * 3);
   image.src = images[random];
 }, 1600);
