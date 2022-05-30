e2a7fd19ef06a8d0b9bdb9675856793fcbe38bb5
Lok <48932138+Lokchin@users.noreply.github.com>
2022-05-30 19:03:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ let images = ['imagemcasa1.png', 'imagemcasa2.jpeg', 'imagemcasa3.jpeg']
 setInterval(async function(){
   let random = Math.floor(Math.random() * 3);
   image.src = images[random];
-}, 1600);
+}, 1000);
